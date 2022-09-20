e1accc07f56e956c6c47c7c2e750d4fa070b7786
Chet Gray <Chet.Gray@waystar.com>
2022-09-20 13:48:27 -0400

    ✨ feat: add a random value to `simonSequence` each loop

--- Program.cs
+++ Program.cs
@@ -1,9 +1,43 @@
-﻿namespace SimonGame
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimonGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<string> simonSequence = new List<string>();
+            int minValue = 0;
+            int maxValue = 9;
+            Random random = new Random();
+
+            bool inputIsCorrect = true;
+            while (inputIsCorrect)
+            {
+                simonSequence.Add(random.Next(minValue, maxValue).ToString());
+                PromptForKeyToContinue();
+            }
+            Console.WriteLine("Incorrect");
+            PromptForKeyToExit();
+        }
+
+        private static void PromptForKeyToContinue(
+            string message = "\nPress any key to continue . . . "
+        )
+        {
+            Console.Write(message);
+            Console.ReadKey(intercept: true);
+            Console.WriteLine();
+        }
+
+        private static void PromptForKeyToExit(
+            string message = "\nPress any key to exit . . . ",
+            int exitCode = 0
+        )
+        {
+            PromptForKeyToContinue(message);
+            Environment.Exit(exitCode);
         }
     }
 }
