9d79fcd244570607d4c8a86a05ae0328214b7ea5
Chet Gray <Chet.Gray@waystar.com>
2022-09-20 16:41:35 -0400

    Merged PR 23640: US211863 Create a Simon Game Console App
    - Add .gitattributes, .gitignore, and README.md.
    - Add project files.
    - ✨ feat: add a random value to `simonSequence` each loop
    - ✨ feat: "Simon says" each item, one at a time, with delay
    - ♻ refactor: extract `SaySequence` method
    - ✨ feat: prompt for each item, break on incorrect
    - ✨ feat: print sequence and score at end
    - 🚸 feat: slight delay between saying items
    - 🐛 fix: only say the new item, not the whole sequence
    - ⏪ revert: "🐛 fix: only say the new item, not the whole sequence"
    
    Closes #211863
    
    Related work items: #211863

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimonGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> simonSequence = new List<string>();
+            List<string> inputSequence = new List<string>();
+            int minValue = 0;
+            int maxValue = 9;
+            int blankDelay = 125;
+            int sayDelay = 3000;
+            Random random = new Random();
+
+            bool inputIsCorrect = true;
+            while (inputIsCorrect)
+            {
+                simonSequence.Add(random.Next(minValue, maxValue).ToString());
+                SaySequence(simonSequence, sayDelay, blankDelay);
+
+                inputSequence.Clear();
+                inputIsCorrect = true;
+                for (int i = 0; i < simonSequence.Count; i++)
+                {
+                    Console.Write($"What was item #{i}\n» ");
+                    inputSequence.Add(Console.ReadLine());
+                    if (inputSequence[i] != simonSequence[i])
+                    {
+                        inputIsCorrect = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Incorrect");
+            Console.WriteLine("Simon\tYou\nsaid:\tsaid:");
+            for (int i = 0; i < simonSequence.Count; i++)
+            {
+                Console.WriteLine($"{simonSequence[i]}\t{inputSequence.ElementAtOrDefault(i)}");
+            }
+            Console.WriteLine($"Your score was: {simonSequence.Count - 1}");
+            PromptForKeyToExit();
+        }
+
+        private static void PromptForKeyToContinue(
+            string message = "\nPress any key to continue . . . "
+        )
+        {
+            Console.Write(message);
+            Console.ReadKey(intercept: true);
+            Console.WriteLine();
+        }
+
+        private static void PromptForKeyToExit(
+            string message = "\nPress any key to exit . . . ",
+            int exitCode = 0
+        )
+        {
+            PromptForKeyToContinue(message);
+            Environment.Exit(exitCode);
+        }
+
+        private static void SaySequence(
+            IEnumerable<string> simonSequence,
+            int sayDelay,
+            int blankDelay = 0
+        )
+        {
+            Console.Clear();
+            foreach (string item in simonSequence)
+            {
+                Console.WriteLine("Simon says:");
+                Task.Delay(blankDelay).Wait();
+                Console.WriteLine(item);
+                Task.Delay(sayDelay).Wait();
+                Console.Clear();
+            }
+        }
+    }
+}
